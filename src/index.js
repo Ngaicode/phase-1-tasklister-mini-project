0a367df2e09c84e266d23208d3ba4d3405e8a1f3
Ngaicode <mjombakevin376@gmail.com>
2022-06-14 08:59:37 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+"use strict";
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // selecting elements from the DOM
+  const addNewTask = document.getElementById("create-task-form");
+
+  //an event listener on the new taskform button
+  addNewTask.addEventListener("submit", createTask);
 });
+const createTask = (event) => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const taskDescription = document.getElementById("new-task-description");
+  const newTaskList = document.createElement("li");
+  newTaskList.innerText = taskDescription.value;
+
+  appendTask(newTaskList);
+  event.target.reset();
+};
+
+const appendTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
